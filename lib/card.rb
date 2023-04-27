08445d09a049aa2c99c2867aa0a551ebd7fd524b
Kayode <bkaoluborode@gmail.com>
2023-04-27 16:01:36 +0100

    feat: create Card and Deck classes

---
+++ lib/card.rb
@@ -0,0 +1,37 @@
+class Card
+    attr_reader :suit, :value, :rank
+    def initialize(suit, value, rank)
+        @suit = suit
+        @value = value
+        @rank = rank
+    end
+end
+
+class Deck
+    attr_reader :cards
+    def initialize(cards)
+        @cards = cards
+    end
+
+    def rank_of_card_at(index)
+        @cards[index].rank
+    end
+
+    def high_ranking_cards()
+        @cards.select do |card|
+            card.rank >= 11
+        end
+    end
+
+    def percent_high_ranking()
+        (high_ranking_cards.length.to_f / @cards.length.to_f * 100).round(2)
+    end
+
+    def remove_card()
+        @cards.shift
+    end
+
+    def add_card(card)
+        @cards << card
+    end
+end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -16,3 +16,38 @@ RSpec.describe Card do
     expect(card.rank).to eq(12)
   end
 end
+
+RSpec.describe Deck do
+  it "exists" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14) 
+
+    cards = [ card1, card2, card3 ]
+
+    deck = Deck.new(cards)
+
+    expect(deck).to be_an_instance_of(Deck)
+  end
+
+  it "returns the correct values" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14) 
+
+    cards = [ card1, card2, card3 ]
+
+    deck = Deck.new(cards)
+
+    expect(deck.rank_of_card_at(0)).to eq(12)
+    expect(deck.high_ranking_cards()).to eq([card1, card3])
+    expect(deck.percent_high_ranking()).to eq(66.67)
+    expect(deck.remove_card()).to eq(card1)
+    
+    card4 = Card.new(:club, '5', 5)
+
+    deck.add_card(card4)
+
+    expect(deck.cards).to eq([card2, card3, card4])
+  end
+end
